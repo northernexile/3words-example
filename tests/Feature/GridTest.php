1f42528f2a8b569ed36fd0ae595a09ec74762f5d
Allen Hardy <anarchalien@outlook.com>
2021-10-14 17:39:42 +0100

    Test for grid

---
+++ tests/Feature/GridTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+
+/**
+ * Class GridTest
+ * @package Tests\Feature
+ */
+class GridTest extends \Tests\TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /** @var User  */
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        Sanctum::actingAs($this->user);
+    }
+
+    /** @test  */
+    public function can_receive_grid_object()
+    {
+        
+    }
+}
